b86093ec2e53aa04c5c9369c040b035b6f73f37c
Nick Karpachev <n.karpachov@gmail.com>
2018-03-10 22:41:19 +0300

    Support multiple translations for Hebrew

--- scripts/make_small_datasets.py
+++ scripts/make_small_datasets.py
@@ -1,6 +1,28 @@
 import argparse
 import numpy as np
 import os
+from collections import defaultdict
+
+
+def save_to_file(data, lang, dir, type):
+    np.savetxt(X=data, fname=os.path.join(
+        dir,
+        "{}.{}.txt".format(lang, type)
+        ),
+        delimiter='\n', fmt='%s', encoding='utf-8'
+    )
+
+
+def make_mapping(first, second):
+    data_first = np.loadtxt(first, dtype='str', encoding='utf-8')
+    data_second = np.loadtxt(second, dtype='str', encoding='utf-8')
+
+    mapping = defaultdict(set)
+
+    for word, translation in zip(data_first, data_second):
+        mapping[word].add(translation)
+
+    return mapping
 
 
 def make_small_datasets(datapath, train_size, test_size, dev_size):
@@ -9,59 +31,57 @@ def make_small_datasets(datapath, train_size, test_size, dev_size):
 
     prefix = datapath.split('/')[-1]
 
+    train_files, langs = [], []
+
     for filename in os.listdir(datapath):
-        new_ix = None
+        if filename.split('.')[1] == 'train':
+            train_files.append(filename)
+            langs.append(filename.split('.')[0])
 
-        try:
-            lang, token = filename.split('.')[:2]
-        except IndexError:
-            continue
+    train_first, train_second = train_files
+    lang_first, lang_second = langs
 
-        if token == 'train':
-            train_data = np.loadtxt(os.path.join(datapath, filename), dtype='str', encoding='utf-8')
+    if train_second.split('.')[0] == 'he':
+        train_first, train_second = train_second, train_first
+        lang_first, lang_second = lang_second, lang_first
 
-            assert (len(train_data) > train_size + test_size + dev_size)
+    mapping = make_mapping(os.path.join(datapath, train_first), os.path.join(datapath, train_second))
 
-            if not new_ix:
-                np.random.seed(0)  # for compatibility
+    total_words = len(mapping)
+    words = np.array(list(mapping.keys()))
 
-                new_ix = np.random.choice(
-                    a=np.arange(len(train_data)), size=train_size + test_size + dev_size, replace=False
-                )
+    print(len(words))
 
-            new_train_data = train_data[new_ix[:train_size]]
-            new_test_data = train_data[new_ix[train_size: -dev_size]]
-            new_dev_data = train_data[new_ix[-dev_size:]]
+    assert (total_words >= train_size + test_size + dev_size)
 
-            new_prefix = "{}-{}k".format(prefix, train_size // 1000)
+    new_ix = np.random.choice(
+        a=np.arange(total_words), size=train_size + test_size + dev_size, replace=False
+    )
 
-            new_datadir = os.path.join('/'.join(datapath.split('/')[:-1]), new_prefix)
+    new_train_words = words[new_ix[:train_size]]
+    new_test_words = words[new_ix[train_size: -dev_size]]
+    new_dev_words = words[new_ix[-dev_size:]]
 
-            if not os.path.exists(new_datadir):
-                os.makedirs(new_datadir)
+    new_train_translations = [mapping[word].pop() for word in new_train_words]
+    new_dev_translations = [mapping[word].pop() for word in new_dev_words]
+    new_test_translations = ['\t'.join([translation for translation in mapping[word]]) for word in new_test_words]
 
-            print("Saving to {}".format(new_datadir))
+    new_prefix = "{}-{}k".format(prefix, train_size // 1000)
 
-            np.savetxt(X=new_train_data, fname=os.path.join(
-                new_datadir,
-                "{}.train.txt".format(lang, train_size, new_prefix)
-                ),
-                delimiter='\n', fmt='%s', encoding='utf-8'
-            )
+    new_datadir = os.path.join('/'.join(datapath.split('/')[:-1]), new_prefix)
 
-            np.savetxt(X=new_test_data, fname=os.path.join(
-                new_datadir,
-                "{}.test.txt".format(lang, train_size, new_prefix)
-                ),
-                delimiter='\n', fmt='%s', encoding='utf-8'
-            )
+    if not os.path.exists(new_datadir):
+        os.makedirs(new_datadir)
 
-            np.savetxt(X=new_dev_data, fname=os.path.join(
-                new_datadir,
-                "{}.dev.txt".format(lang, train_size, new_prefix)
-                ),
-                delimiter='\n', fmt='%s', encoding='utf-8'
-            )
+    print("Saving to {}".format(new_datadir))
+
+    save_to_file(new_train_words, lang_first, new_datadir, 'train')
+    save_to_file(new_test_words, lang_first, new_datadir, 'test')
+    save_to_file(new_dev_words, lang_first, new_datadir, 'dev')
+
+    save_to_file(new_train_translations, lang_second, new_datadir, 'train')
+    save_to_file(new_test_translations, lang_second, new_datadir, 'test')
+    save_to_file(new_dev_translations, lang_second, new_datadir, 'dev')
 
 
 if __name__ == '__main__':
