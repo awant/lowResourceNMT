c84c9d1af109f580c7597bab08b5c463a669b782
Roman <roman.marakulin2015@yandex.ru>
2018-03-11 06:47:49 +0300

    simplify script. now you need only data in appropriate place

--- scripts/he-en_translation.sh
+++ scripts/he-en_translation.sh
@@ -1,27 +1,34 @@
-# 1) set ROOT_DATA_DIR (should point to folder from git repo: ROOT_DATA_DIR=/media/roman/Elements/LowResourceNMT)
-# 2) download he-en data to DATA_DIR path (should exists: DATA_DIR/en.train.txt, DATA_DIR/en.test.txt for training)
-# 3) set TRAIN_SIZE (in 1000, for example: 236 means 236k. 236 is about 80% of all dataset, set TRAIN_SIZE <= 236 or change test/dev sizes)
-# 4) run this script
+# 1) download he-en data to DATA_DIR path (should exists: DATA_DIR/en.train.txt, DATA_DIR/en.test.txt for training)
+# 2) set TRAIN_SIZE (in 1000, for example: 236 means 236k. 236 is about 80% of all dataset, set TRAIN_SIZE <= 236 or change test/dev sizes)
+# 3) run this script
 
-ROOT_DATA_DIR=/media/roman/Elements/LowResourceNMT
 TRAIN_SIZE=236
 
 PROBLEM=translate_he_to_en
 MODEL=transformer
 HPARAMS=transformer_enhe
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DATA_DIR=$SCRIPT_DIR/..
+
 DATA_DIR=$ROOT_DATA_DIR/data/t2t_data
 TMP_DIR=$ROOT_DATA_DIR/data/t2t_datagen
 TRAIN_DIR=$ROOT_DATA_DIR/data/t2t_train/$PROBLEM/$MODEL-$HPARAMS
 USR_DIR=$ROOT_DATA_DIR/translate_enhe
-DECODE_FILE=$DATA_DIR/he.test.txt
-TEST_REFS=$DATA_DIR/en.test.txt # several refs can be separated by tabs
 BEAM_SIZE=4
 ALPHA=0.6
 
+if [ ! -f $DATA_DIR/he.train.txt ] || [ ! -f $DATA_DIR/en.train.txt ]; then
+    echo "Train data doesn't exist!"
+    exit
+fi
+
 # Split train-dev-test data, train/dev/test in k: 10k means 10000
 python3 $ROOT_DATA_DIR/scripts/make_small_datasets.py --datadir=$DATA_DIR --train_size=$TRAIN_SIZE --test_size=29 --dev_size=29
+
 DATA_DIR=$ROOT_DATA_DIR/data/t2t_data-${TRAIN_SIZE}k
+DECODE_FILE=$DATA_DIR/he.test.txt
+TEST_REFS=$DATA_DIR/en.test.txt # several refs can be separated by tabs
 
 # Generate data
 T2T_DATAGEN_PATH=t2t-datagen
@@ -56,8 +63,8 @@ t2t-decoder \
 # Compute bleu
 python3 $ROOT_DATA_DIR/scripts/compute_bleu.py --translation=$DATA_DIR/he-to-en.translit.txt --reference=$TEST_REFS
 
-
 # For troubles:
 # check that problem/hparams registered with (find your mods):
 # t2t-trainer --t2t_usr_dir=$USR_DIR --registry_help
 # If you run out of memory, when train: add --hparams='batch_size=1024'
+
