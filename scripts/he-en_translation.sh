6edb465c2e2d5a816a79af7b1b559892f91f7f47
zakharovas <alexkru2011@gmail.com>
2018-03-12 15:25:47 +0300

    Script options added

--- scripts/he-en_translation.sh
+++ scripts/he-en_translation.sh
@@ -3,20 +3,64 @@
 # 3) run this script
 
 TRAIN_SIZE=236
-
+TEST_SIZE=29
+DEV_SIZE=29
 PROBLEM=translate_he_to_en
 MODEL=transformer
 HPARAMS=transformer_enhe
+BATCH_SIZE=4096
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DATA_DIR=$SCRIPT_DIR/..
-
 DATA_DIR=$ROOT_DATA_DIR/data/t2t_data
 TMP_DIR=$ROOT_DATA_DIR/data/t2t_datagen
 TRAIN_DIR=$ROOT_DATA_DIR/data/t2t_train/$PROBLEM/$MODEL-$HPARAMS
 USR_DIR=$ROOT_DATA_DIR/translate_enhe
 BEAM_SIZE=4
 ALPHA=0.6
+TRAIN_STEPS=10000
+
+while [ -n "$1" ]
+do
+echo "MENE $1"
+case "$1" in
+--train_size) TRAIN_SIZE=$2
+shift;;
+--test_size) TEST_SIZE=$2
+shift;;
+--dev_size) DEV_SIZE=$2
+shift;;
+
+--model) MODEL=$2
+shift;;
+--problem) PROBLEM=$2
+shift;;
+--hparams) HPARAMS=$2
+shift;;
+
+--batch_size) BATCH_SIZE=$2
+shift;;
+--train_steps) TRAIN_STEPS=$2
+shift;;
+
+
+
+--data_dir) DATA_DIR=$2
+shift;;
+--usr_dir) USR_DIR=$2
+shift;;
+
+--beam_size) BEAM_SIZE=$2
+shift;;
+
+--alpha) ALPHA=$2
+shift;;
+
+*) echo "$1 unknown option"
+exit 1;;
+esac
+shift
+done
 
 if [ ! -f $DATA_DIR/he.train.txt ] || [ ! -f $DATA_DIR/en.train.txt ]; then
     echo "Train data doesn't exist!"
@@ -24,7 +68,7 @@ if [ ! -f $DATA_DIR/he.train.txt ] || [ ! -f $DATA_DIR/en.train.txt ]; then
 fi
 
 # Split train-dev-test data, train/dev/test in k: 10k means 10000
-python3 $ROOT_DATA_DIR/scripts/make_small_datasets.py --datadir=$DATA_DIR --train_size=$TRAIN_SIZE --test_size=29 --dev_size=29
+python3 $ROOT_DATA_DIR/scripts/make_small_datasets.py --datadir=$DATA_DIR --train_size=$TRAIN_SIZE --test_size=$TEST_SIZE --dev_size=$DEV_SIZE
 
 DATA_DIR=$ROOT_DATA_DIR/data/t2t_data-${TRAIN_SIZE}k
 DECODE_FILE=$DATA_DIR/he.test.txt
@@ -44,8 +88,9 @@ t2t-trainer \
   --problems=$PROBLEM \
   --model=$MODEL \
   --hparams_set=$HPARAMS \
+  --hparams='batch_size=$BATCH_SIZE' \
   --output_dir=$TRAIN_DIR \
-  --train_steps=10000 \
+  --train_steps=$TRAIN_STEPS \
   --t2t_usr_dir=$USR_DIR
 
 # Compute transliterations
