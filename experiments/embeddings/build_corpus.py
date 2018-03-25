4f3c58e20274970cac2e3f91049c5d4323bed9a8
sergmiller <sergeymiller1996@instance-1.c.hallowed-tea-195612.internal>
2018-03-25 13:49:50 +0000

    add embeddings processing files

---
+++ experiments/embeddings/build_corpus.py
@@ -0,0 +1,183 @@
+# coding: utf-8
+#!/usr/bin/python2
+import argparse
+import codecs
+import lxml.etree as ET
+import os
+import regex
+import numpy as np
+
+from tokenizer import SentenceTokenizer
+
+
+DUMP_DATE = "20180301"
+
+# arguments setting
+parser = argparse.ArgumentParser()
+parser.add_argument('--lcode', help='ISO 639-1 code of target language.')
+parser.add_argument('--part', type=float, default=1, help='part of corpus')
+parser.add_argument('--workers', type=int, default=3, help='the number of workers')
+parser.add_argument('--max_corpus_size', type=int, default=1000000000, help='the maximum size of the corpus. Feel free to adjust it according to your computing power.')
+args = parser.parse_args()
+workers = args.workers
+part = args.part
+
+ST = SentenceTokenizer(lang=args.lcode)
+
+lcode = args.lcode
+if lcode == 'ko':
+    from konlpy.tag import Kkma # pip install konlpy. See http://konlpy.org/en/v0.4.4/ for further information.
+    kkma = Kkma()
+elif lcode == 'ja':
+    import MeCab # See https://pypi.python.org/pypi/mecab-python/0.996
+    mecab = MeCab.Tagger("-Owakati")
+elif lcode == 'zh':
+    import jieba # See https://pypi.python.org/pypi/jieba/
+elif lcode == 'vi':
+    from pyvi.pyvi import ViTokenizer # See https://pypi.python.org/pypi/pyvi
+elif lcode == 'th':
+    import pythai # See https://pypi.python.org/pypi/pythai
+# elif lcode == 'ar':
+#     os.environ['CLASSPATH'] = "../stanford-segmenter-2015-12-09"
+#     from nltk.tokenize.stanford_segmenter import StanfordSegmenter
+#     segmenter = StanfordSegmenter(path_to_jar="../stanford-segmenter-2015-12-09/stanford-segmenter-3.6.0.jar",
+#                                path_to_sihan_corpora_dict="../stanford-segmenter-2015-12-09/data",
+#                                path_to_model="../stanford-segmenter-2015-12-09/data/pku.gz",
+#                                path_to_dict="../stanford-segmenter-2015-12-09/data/dict-chris6.ser.gz")
+
+max_corpus_size = args.max_corpus_size
+fname = "{}wiki-{}-pages-articles-multistream.xml".format(lcode, DUMP_DATE)
+
+def clean_text(text):
+    global lcode
+
+    # Common
+    text = regex.sub("(?s)<ref>.+?</ref>", "", text) # remove reference links
+    text = regex.sub("(?s)<[^>]+>", "", text) # remove html tags
+    text = regex.sub("&[a-z]+;", "", text) # remove html entities
+    text = regex.sub("(?s){{.+?}}", "", text) # remove markup tags
+    text = regex.sub("(?s){.+?}", "", text) # remove markup tags
+    text = regex.sub("(?s)\[\[([^]]+\|)", "", text) # remove link target strings
+    text = regex.sub("(?s)\[\[([^]]+\:.+?]])", "", text) # remove media links
+
+    text = regex.sub("[']{5}", "", text) # remove italic+bold symbols
+    text = regex.sub("[']{3}", "", text) # remove bold symbols
+    text = regex.sub("[']{2}", "", text) # remove italic symbols
+
+    if lcode in ['ko']: # korean
+        text = regex.sub(u"[^ \r\n\p{Hangul}.?!]", " ", text) # Replace unacceptable characters with a space.
+    elif lcode in ['ja']: # japanese
+        text = regex.sub(u"[^\r\n\p{Han}\p{Hiragana}\p{Katakana}ー。！？]", "", text)
+    elif lcode in ['zh']: # chinsese
+        text = regex.sub(u"[^\r\n\p{Han}。！？]", "", text)
+    elif lcode in ['th']: # thai
+        text = regex.sub(u"[^ \r\n\p{Thai}.?!]", " ", text)
+    elif lcode in ['ru']: # russian
+        text = regex.sub(u"[^ \r\n\p{Cyrillic}.?!\-]", " ", text)
+        text = text.lower()
+#     elif lcode in ['ar']: # arabic
+#         text = regex.sub(u"[^ \r\n\p{Arabic}.?!\-]", " ", text)
+    elif lcode in ['hi']: # hindi
+        text = regex.sub(u"[^ \r\n\p{Devanagari}.।?!\-]", " ", text)
+    elif lcode in ['bn']: # bengali
+        text = regex.sub(u"[^ \r\n\p{Bengali}.।?!\-]", " ", text)
+    elif lcode in ['de']: # german
+        text = regex.sub(u"[^ \r\n\p{Latin}\-'‘’.?!]", " ", text)
+    else: # Mostly european languages
+        text = regex.sub(u"[^ \r\n\p{Latin}\-'‘’.?!]", " ", text)
+        text = text.lower()
+
+    # Common
+    text = regex.sub("[ ]{2,}", " ", text) # Squeeze spaces.
+    return text
+
+def sentence_segment(text):
+    '''
+    Args:
+      text: A string. A unsegmented paragraph.
+
+    Returns:
+      A list of sentences.
+    '''
+    global lcode
+    if lcode in ['ja', 'zh']:
+        sents = regex.split(u"([。！？])?[\n]+|[。！？]", text)
+    elif lcode in ['th']:
+        sents = text.split("[\n]+")
+    elif lcode in ['hi', 'bn']: # hindi, bengali
+        sents = regex.split(u"([.।?!])?[\n]+|[.।?!] ", text)
+    elif lcode in ['de']: # german
+        sents = regex.split("([.?!])?[\n]+|[.?!] ", text)
+        sents = [sent[0].lower() + sent[1:] for sent in sents if sent is not None and len(sent) > 1]
+    else:
+        sents = regex.split("([.?!])?[\n]+|[.?!] ", text)
+    return sents
+
+def word_segment(sent):
+    '''
+    Args:
+      sent: A string. A sentence.
+
+    Returns:
+      A list of words.
+    '''
+    global lcode, ST
+
+    if lcode in ['ko']:
+        words = [word for word, _ in kkma.pos(sent)]
+    elif lcode in ['ja']:
+        words = mecab.parse(sent.encode('utf8')).split()
+    elif lcode in ['th']:
+        words = pythai.split(sent)
+    elif lcode in ['vi']:
+        words = ViTokenizer.tokenize(sent).split()
+    elif lcode in ['zh']:
+        words = list(jieba.cut(sent, cut_all=False))
+#     elif lcode in ['ar']:
+#         words = segmenter.segment(sent).split()
+    else: # Mostly european languages
+        words = sent.split()
+
+    words = ST.tokenize(words)
+
+    return words
+
+
+def build_text(text):
+    if not np.random.binomial(n=1, p=part):
+        return
+    try:
+        text = clean_text(text)
+        sents = sentence_segment(text)
+        sents = [word_segment(sent) for sent in sents if sent is not None]
+        return [sent for sent in sents if len(sent) > 10]
+    except:
+        return
+
+
+def build_corpus():
+    global lcode, max_corpus_size, fname
+    from joblib import Parallel, delayed
+    from tqdm import tqdm
+
+
+    ns = "{http://www.mediawiki.org/xml/export-0.10/}" # namespace
+    with Parallel(n_jobs=workers) as parallel:
+        texts = parallel(delayed(build_text)(elem.text)
+    for _, elem in tqdm(ET.iterparse("data/{}".format(fname), tag=ns+"text")))
+
+    with codecs.open("data/{}.txt".format(lcode), 'w', 'utf-8') as fout:
+        for sents in texts:
+            if sents is None:
+                continue
+            for words in sents:
+                if lcode in ['ja']:
+                    fout.write(" ".join(words).decode('utf8') + "\n")
+                else:
+                    fout.write(" ".join(words) + "\n")
+
+
+if __name__ == "__main__":
+    build_corpus()
+
+    print("Done")
---
+++ experiments/embeddings/make_wordvectors.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#### Set your hyper-parameters here ####
+############## START ###################
+lcode=$1 # ISO 639-1 code of target language. See `lcodes.txt`. example: ru, en
+max_corpus_size=1000000000 # the maximum size of the corpus. Feel free to adjust it according to your computing power.
+vector_size=300 # the size of a word vector
+window_size=5 # the maximum distance between the current and predicted word within a sentence.
+vocab_size=20000 # the maximum vocabulary size
+num_negative=5 # the int for negative specifies how many “noise words” should be drawn
+workers=24 # u definetily need to change that number
+part=0.0001
+############## END #####################
+
+echo "step 0. Make 'data' directory."
+mkdir data;
+
+echo "step 1. Build Corpus."
+python build_corpus.py --part=${part} --lcode=${lcode} --max_corpus_size=${max_corpus_size} --workers=24
+
+echo "step 2. make wordvectors"
+python make_wordvectors.py  --workers=${workers} --lcode=${lcode} \
+                    --vector_size=${vector_size} --window_size=${window_size} \
+                    --vocab_size=${vocab_size} --num_negative=${num_negative}
