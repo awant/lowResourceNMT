5846ea52710928725969a639f35bc1fa8437e298
Nick Karpachev <n.karpachov@gmail.com>
2018-03-26 13:48:42 +0300

    Update tokenizer

--- experiments/embeddings/tokenizer.py
+++ experiments/embeddings/tokenizer.py
@@ -1,51 +1,55 @@
 from nltk.stem.snowball import RussianStemmer, EnglishStemmer
 
 class SentenceTokenizer(object):
-    def __init__(self, lang, make_delimiters=True):
-        self.__stemmers = {
-            'en': EnglishStemmer(),
-            'ru': RussianStemmer(),
-        }
+	'''
+	Tokenizes sentences using SnowBall Stemmer
+	(each word is split into stem and suffix deterministically)
+	'''
+	def __init__(self, lang, make_delimiters=True):
+		self.__stemmers = {
+			'en': EnglishStemmer(),
+ 			'ru': RussianStemmer(),
+		}
 
-        if lang not in ('en', 'ru'):
-            raise ValueError
+		if lang not in ('en', 'ru'):
+			raise ValueError
 
-        self.__active_stemmer = self.__stemmers[lang]
-        self.__make_delimiters = make_delimiters
-
-
-    def tokenize(self, sentence):
-        tokens = [self.__tokenize_word(word) for word in sentence]
-
-        tokenized_sentence = [_ for token in tokens for _ in token]
-
-        if self.__make_delimiters:
-            tokenized_sentence = ['<s>'] + tokenized_sentence + ['<\s>']
-
-        return tokenized_sentence
+		self.__active_stemmer = self.__stemmers[lang]
+		self.__make_delimiters = make_delimiters
 
 
-    def detokenize(self, tokens):
-        source_text = ''
-        for token in tokens:
-            source_text += self.__detokenize_token(token)
+	def tokenize(self, sentence):
+		tokens = [self.__tokenize_word(word) for word in sentence]
 
-        return source_text.lstrip()
+		tokenized_sentence = [_ for token in tokens for _ in token]
 
+		if self.__make_delimiters:
+			tokenized_sentence = ['<s>'] + tokenized_sentence + ['<\s>']
 
-    def __tokenize_word(self, word):
-        stem = self.__active_stemmer.stem(word)
-        affix = word[len(stem):]
-
-        if affix:
-            return (stem, '#' + affix)
-        else:
-            return (stem, )
+		return tokenized_sentence
 
 
-    def __detokenize_token(self, token):
-        if token.startswith('#'):
-            return token[1:]
-        else:
-            return ' ' + token 
+	def detokenize(self, tokens):
+		source_text = ''
+		for token in tokens:
+			source_text += self.__detokenize_token(token)
+
+		return source_text.lstrip()
+
+
+	def __tokenize_word(self, word):
+		stem = self.__active_stemmer.stem(word)
+		affix = word[len(stem):]
+
+		if affix:
+ 			return (stem, '#' + affix)			#To make embeddings work, all suffixes start with artificial token '#'
+		else:
+			return (stem, )
+
+
+	def __detokenize_token(self, token):
+		if token.startswith('#'):
+			return token[1:]
+		else:
+			return ' ' + token 
 
